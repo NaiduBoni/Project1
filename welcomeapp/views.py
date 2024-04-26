491d8cd358da6755e7ce5f32fd712f9409fe7a6c
NaiduBoni <naiduboni23@gmail.com>
2024-04-26 12:07:11 +0530

    Initial commit

---
+++ welcomeapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    res = HttpResponse("""<html><body bgcolor=lightblue><center>
+    <h1>welcome to lokesh it</h1></center></body></html>""")
+    return res
